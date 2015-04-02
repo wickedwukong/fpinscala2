c4979198579374d7ffd44e71042dedc5409025c5
Rhys Keepence <rhys.keepence@db.com>
2015-04-02 15:38:19 +0100

    Implemented foldRight using foldMap.

--- exercises/src/main/scala/fpinscala/monoids/Monoid.scala
+++ exercises/src/main/scala/fpinscala/monoids/Monoid.scala
@@ -81,10 +81,10 @@ object Monoid {
         (b, a) => m.op(b, f(a))
       }
     }
-  //
-  //  def foldRight[A, B](as: List[A])(z: B)(f: (A, B) => B): B =
-  //    sys.error("todo")
-  //
+
+    def foldRight[A, B](as: List[A])(z: B)(f: (A, B) => B): B =
+      foldMap(as, endoMonoid[B])(f.curried)(z)
+
   //  def foldLeft[A, B](as: List[A])(z: B)(f: (B, A) => B): B =
   //    sys.error("todo")
   //
