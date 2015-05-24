737a3c86d7b0e44cd9d257fc3cd28606b5a25916
Xuemin Guan <gxuemin@gmail.com>
2015-05-24 18:46:05 +0100

    implemented map2 using apply@

--- exercises/src/main/scala/fpinscala/applicative/Applicative.scala
+++ exercises/src/main/scala/fpinscala/applicative/Applicative.scala
@@ -10,7 +10,7 @@ import monoids._
 trait Applicative[F[_]] extends Functor[F] {
 //apply(F[(A, B) => C])(F[(A, B)])
   def map2[A,B,C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] = {
-    
+    apply(map(fa)(f.curried))(fb)
 }
 
   def apply[A,B](fab: F[A => B])(fa: F[A]): F[B] = map2(fab, fa)((ab, a) => ab(a))
